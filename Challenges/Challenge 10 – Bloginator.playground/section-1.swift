48838863400ff587b898fa21733a4211a9c06fd3
Mixail Muravev <mixaillanatolich@gmail.com>
2014-08-26 22:41:18 +0400

    implemented R3

--- Challenges/Challenge 10 – Bloginator.playground/section-1.swift
+++ Challenges/Challenge 10 – Bloginator.playground/section-1.swift
@@ -37,7 +37,7 @@ class BlogPost {
     
     func teaser() -> String {
         if let viewsCount = views {
-            return "Mike Smith has \(views) views for this blog post"
+            return "Mike Smith has \(viewsCount) views for this blog post"
         } else {
             return "No one has read your blog post yet. Pity."
         }
@@ -50,4 +50,78 @@ an “Author” struct with a “name” and “email” property. Add 5 Author 
 to a dictionary using the author’s name as the key. This function returns
 a randomly selected name and email of an Author from the dictionary.
 **/
+
+func randomAuthor() -> (String, String) {
+    
+    struct Author {
+        var name: String
+        var email: String
+    }
+    
+    let author1 = Author(name: "FirstName LastName I", email: "author1@mail.com")
+    let author2 = Author(name: "FirstName LastName II", email: "author2@mail.com")
+    let author3 = Author(name: "FirstName LastName III", email: "author3@mail.com")
+    let author4 = Author(name: "FirstName LastName IV", email: "author4@mail.com")
+    let author5 = Author(name: "FirstName LastName V", email: "author5@mail.com")
+    
+    var authors = Dictionary<String, Author>()
+    authors[author1.name] = author1
+    authors[author2.name] = author2
+    authors[author3.name] = author3
+    authors[author4.name] = author4
+    authors[author5.name] = author5
+    
+    let selectedAuthor = authors.values.array[Int(arc4random_uniform(UInt32(authors.count)))]
+    
+    return (selectedAuthor.name, selectedAuthor.email)
+}
+
+/**
+R4 – Create an array of 10 blog posts with a for loop. Set the “author”
+property to the value returned from “randomAuthor” function and set
+the “order” property to the value of the index of the for loop used to
+populate this array.
+**/
+// implement code for R4 below
+
+
+
+/**
+R5 – Create a “randomViews” function that accepts the “type” of blog post
+for an input. Create an array literal of Ints (these are the possible view
+counts) with the values: 10, 20, 30, 40, 50. Select a random value from
+this possible view count array and create a switch that returns nil if
+the views are 10, 0 if the views are either 20 or 30, else return the randomly
+selected number of views. If the “type” passed to the function is
+“cat videos” always return nil.
+**/
+// implement code for R5 below
+
+
+
+/**
+R6 – Iterate the array of blog posts, generate a value from “randomViews” function
+for each one and set it to the blog post “views” property. Then println the
+value of each post.teaser().
+**/
+// implement code for R6 below
+
+
+
+/**
+R7 – Write a simple stack using a struct with Generics
+that has three methods. It should push and pop BlogPosts and also
+returns the current count of BlogPosts. Iterate the array of blog posts
+and push each one onto the “blogs” stack. Println the count of items in
+the stack. Use a for loop to iterate the “blogs” stack, pop the top one
+off and println the blog’s teaser. Println the count of items in the stack.
+**/
+// implement code for R7 below
+
+
+
+
+
+
+
 
